3215f9b3fd80548afadba756c8e85328ec2e2cab
bee-mcc <tuj03791@temple.edu>
2021-10-19 15:02:49 -0400

    began switch of logic toward php bruteforce / defined gen_keywords

--- archives_crawler/archives_crawler/spiders/archives_spider_lachlain.py
+++ archives_crawler/archives_crawler/spiders/archives_spider_lachlain.py
@@ -3,8 +3,27 @@ import scrapy
 
 class ArchivesSpiderLachlainSpider(scrapy.Spider):
     name = 'archives_spider_lachlain'
-    allowed_domains = ['https://asb.nadir.org/archivsystematik.php']
-    start_urls = ['http://https://asb.nadir.org/archivsystematik.php/']
+    allowed_domains = ['asb.nadir.org']
+    start_urls = ['https://asb.nadir.org/tp1.php?urlpara=1000']
+    current_url = 'https://asb.nadir.org/tp1.php?urlpara=1000'
 
     def parse(self, response):
-        pass
+
+        keywords = gen_keywords()
+        
+        for keyword in keywords:
+
+            relevantContent = response.xpath("//*[contains(text(), 'MY TEXT')]").getall()
+            for item in relevantContent:
+                yield{
+
+                    "url" : self.current_url,
+                    "Keywords triggered" : item.extract()
+
+                }
+
+
+    def gen_keywords():
+        wordArray = []
+        return wordArray
+
