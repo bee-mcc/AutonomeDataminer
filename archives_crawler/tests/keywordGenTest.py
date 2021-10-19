5510ae6e959772ee631fb67d95d3c1e9ace49ea0
bee-mcc <tuj03791@temple.edu>
2021-10-19 15:25:04 -0400

    added test logic

--- archives_crawler/tests/keywordGenTest.py
+++ archives_crawler/tests/keywordGenTest.py
@@ -1,6 +1,25 @@
 import unittest
-
+from unittest.main import main
 
 target = __import__("archives_spider_lachlain.py")
-sum = target.gen_keywords
+gen_keywords = target.gen_keywords
 
+class TestGenkeywords(unittest.TestCase):
+
+    def test_keyword_list(self):
+        """
+        Test that it can sum a list of integers
+        """
+        data = ["Kartoffel", "Stein", "Fahrrad"]
+
+        result = gen_keywords("test.txt")
+        self.assertEqual(result, data)
+
+
+    if __name__ == '__main__':
+        unittest.main()
+        
+        
+
+
+    
